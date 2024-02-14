40102c60f7d05933f8cbac7d470c5af76273df37
Alin Ciobanu <alinviorelciobanu@gmail.com>
2024-02-14 10:49:44 +0200

    Add script

---
+++ Script.js
@@ -0,0 +1,17 @@
+let number = Math.floor(Math.random() * 6);
+
+switch (number)
+{
+    case 0: console.log('The life is good');
+            break;
+    case 1: console.log('Hello honey!');
+            break;
+    case 2: console.log('Programming is a plesure');
+            break;
+    case 3: console.log('Go with me in the amazing world of programming');
+            break;
+    case 4: console.log('Happy Valentines Day!!!');
+            break;
+    case 5: console.log('I LOVE YOU!!!<3');
+            break;
+}

d727e10ee593397de7963c92372229f679d70b43
Alin Ciobanu <alinviorelciobanu@gmail.com>
2024-02-15 09:23:03 +0200

    The final change

--- Script.js
+++ Script.js
@@ -1,6 +1,18 @@
+const arr1 = ['valentines day', 'love', 'passion', 'flowers', 'night'];
+const arr2 = ['is the one', 'oulalala', 'moments', 'fall in'];
+const arr3 = ['just one per year', 'I love you', 'with you', 'bed', 'your arms'];
 let number1 = Math.floor(Math.random() * arr1.length);
 let number2 = Math.floor(Math.random() * arr2.length);
 let number3 = Math.floor(Math.random() * arr3.length);
+
+const arrOfMessages = [arr1, arr2, arr3];
+let numberArray = Math.floor(Math.random()* arrOfMessages.length);
+const firstWordChanger = (string) =>{
+        let firstLetter = string[0].toUpperCase();
+        string = firstLetter + string.slice(1);
+        
+        return string;
+     };
 /*
 switch (number)
 {
@@ -17,12 +29,32 @@ switch (number)
     case 5: console.log('I LOVE YOU!!!<3');
             break;
 }*/
+        //console.log(arr1[number1] + ' ' + arr2[number2] + ' ' + arr3[number3]);
 
-const arr1 = ['valentines day', 'love', 'passion', 'flowers', 'night'];
-const arr2 = ['is the one', 'Oulalala', 'moments', 'fall in'];
-const arr3 = ['just one per year', 'I love you', 'with you', 'bed', 'your arms'];
+const randomMessage = (array, num1, num2, num3) =>{
+     let num = Math.floor(Math.random()* array.length);
+     let messageText = "";
+     
+     let firstWord = firstWordChanger( array[num][num1]);
+     messageText += firstWord;
+     let mirror1 = num;
+     while(num === mirror1)
+     {
+        num = Math.floor(Math.random()* array.length);
+     } 
 
-for(let i = 0; i < arr1.length; i++)
-{
-        console.log(arr1[number1] + ' ' + arr2[number2] + ' ' + arr3[number3]);
-}
+     let mirror2 = num;
+     messageText += ' ' + array[num][num2];
+
+     while(num === mirror1 || num === mirror2)
+     {
+        num = Math.floor(Math.random()* array.length);
+     } 
+
+     messageText += ' ' + array[num][num3];
+     return messageText;
+};
+
+console.log(randomMessage(arrOfMessages, number1,number2,number3));
+
+
